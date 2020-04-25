6b337a264408a3dfe28bd229a2d2ab0a040a4886
NoemaSiokapesi18 <55466262+NoemaSiokapesi18@users.noreply.github.com>
2020-04-25 15:43:56 -0700

    commit

---
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My name is Pesi + And I am from Seattle!");
+            string date = DateTime.UtcNow.ToString("MM-dd-yyyy");
+            Console.WriteLine("The current date is {0}", date);
+           
+         
+        }
+       
+    }
+}
