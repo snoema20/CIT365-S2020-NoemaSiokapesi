6bdd1b7d1e34b17460935ef63b7f53da4de59547
NoemaSiokapesi18 <55466262+NoemaSiokapesi18@users.noreply.github.com>
2020-05-06 22:20:47 -0700

    commit

--- WindowsFormsApp1/WindowsFormsApp1/MakeQuote.cs
+++ WindowsFormsApp1/WindowsFormsApp1/MakeQuote.cs
@@ -2,13 +2,50 @@
 
 namespace MegaDesk_Noema
 {
-    internal class MakeQuote
+    public partial class MakeQuote : Form
     {
-        public Form1 Tag { get; internal set; }
-
-        internal void Show()
+        public MakeQuote()
         {
-            throw new NotImplementedException();
+            InitializeComponent();
+        }
+
+        private void CancelNewForm_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void MakeQuote_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            HomeForm homeForm = (HomeForm)Tag;
+            homeForm.Show();
+        }
+
+        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListBox listBox = (ListBox)sender;
+            string material = (string)listBox.SelectedItem;
+
+
+            if (material == "Oak")
+            {
+                this.SamplePic.Load("C:\\Users\\Lazjh\\Documents\\Spring 2019\\CIT 365\\oak.jpg");
+            }
+            else if (material == "Laminate")
+            {
+                this.SamplePic.Load("C:\\Users\\Lazjh\\Documents\\Spring 2019\\CIT 365\\laminate.jpg");
+            }
+            else if (material == "Rosewood")
+            {
+                this.SamplePic.Load("C:\\Users\\Lazjh\\Documents\\Spring 2019\\CIT 365\\rosewood.jpg");
+            }
+            else if (material == "Pine")
+            {
+                this.SamplePic.Load("C:\\Users\\Lazjh\\Documents\\Spring 2019\\CIT 365\\pine.jpg");
+            }
+            else if (material == "Veneer")
+            {
+                this.SamplePic.Load("C:\\Users\\Lazjh\\Documents\\Spring 2019\\CIT 365\\veneer.jpg");
+            }
         }
     }
 }
