7e9c2ac863532964dd539b98a9c16d82dbecdc51
NoemaSiokapesi18 <55466262+NoemaSiokapesi18@users.noreply.github.com>
2020-05-09 12:57:41 -0700

    commit

--- Enhancement/HomePage.php
+++ Enhancement/HomePage.php
@@ -1,63 +1,67 @@
-<!doctype html>
+<!Doctype HTML>
 <html lang="en">
-
-<head>
- <meta charset="utf-8">
- <meta http-equiv="x-ua-compatible" content="ie=edge">
- <title>Enhancement 1 | Web Backend Development I</title>
- <meta name="description" content="CSE 340 Enhancement 1 ">
- <meta name="viewport" content="width=device-width">
- <link rel="stylesheet" href="#" media="screen">
-</head>
-
-<body>
- <div id="wrapper">
-  <header id="header"><img src="/#" alt="Banner"></header>
-  <main>
-   
-    <h1>Enhancement 1</h1>
-    <h2>#</h2>
-    <p>#</p>
-
-    <h2>#</h2>
-    <p>#</p>
-
-    <h3>#</h3>
-    <p>#</p>
-     <iframe src="#"></iframe>
-
-    <h2>#</h2>
-    <p>#</p>
-
-    <h2>#</h2>
-    <p>#</p>
-
-    <h2>$</h2>
-    <h3>#</h3>
-    <p>#<br>
-     <img src="#"></p>
-
-    <p>#</p>
-    
-     <li>#</li>
-     <li>#</li>
-     <li>#</li>
-     <li>#</li>
-     <li>#</li>
-     <li>#</li>
-     <li>#</li>
-     <li>#</li>
-     <li>#</li>
-     <li>#</li>     
-
-    <h2>#</h2>
-    <ul>#</ul>
-    <h2>#</h2>
-    <p>#</p>
-    <ul>#</ul>
-   
-  </main>
- </div>
-</body>
-
+        
+     <head>
+               
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Siokapesi Noema: CIT 230 Assignments</title>
+                <meta name="description" content="Assignment portal for Siokapesi Noema in CIT 230: Web Frontend Development at Brigham Young University - Idaho">
+                
+              
+             
+                <link ref="stylesheet" href="css/normalize.css">
+                <link rel="stylesheet" href="css/small.css">       
+                <link rel="stylesheet" href="css/medium.css">
+                
+  
+        </head>
+      
+         
+            <body>
+                
+                <header>
+            
+                        <img src="images.jpg/profile.jpg" alt="Profile picture">
+                        <h1>Siokapesi Noema</h1> 
+                        </header>   
+                  <!--<nav>
+                  </nav> -->
+                
+                
+                <main>
+                        <section>
+                                <h2>CIT 230: Assignment Portal</h2>
+                                <hr>
+                                <ul>
+           <li>Lesson 02: <a href="HomePage.php">Assignment Link</a></li>  
+           <li>Lesson 03: <a href="Lesson 3/index.html">Assignment Link</a></li>    
+           <li>Lesson 04: <a href="Lesson 4/Preston-4.html">Assignment Link</a></li>    
+           <li>Lesson 05: <a href="Lesson 5/Preston-5.html">Assignment Link</a></li> 
+           <li>Lesson 06: <a href="Lesson 6/Preston 6.html">Assignment Link</a></li>     
+           <li>Lesson 07: <a href="Lesson 7/lazy-loading.html">Assignment Link - Lazy Loading</a></li> 
+           <li>Lesson 07: <a href="Lesson 7/Galley.html">Assignment Link - Galley</a></li>    
+           <li>Lesson 08: <a href="Lesson 8/Table Build.html">Assignment Link - Table</a></li>   
+           <li>Lesson 08: <a href="Lesson 8/storm.html">Assignment Link - Storm</a></li>
+           <li>Lesson 09: <a href="Lesson 9/Prophet.html">Assignment Link - Prophets</a></li>
+           <li>Lesson 09: <a href="Lesson 9/Index-9.html">Assignment Link - Preston-9</a></li>
+           <li>Lesson 10: <a href="Lesson 10/Preston 10.html">Assignment Link - Preston-10</a></li>
+           <li>Lesson 11: <a href="Lesson 11/Preston 11.html">Assignment Link - Preston-11</a></li>
+           <li>Lesson 12: <a href="Lesson 12/index.html">Assignment Link - Lesson 12</a></li>
+           <li>Lesson 14: <a href="Final Project/index.html">Assignment Link - Final Project</a></li>
+           
+          
+            </ul>
+            </section>
+        </main>
+        <footer>
+            <p>&copy; <span id="copyright"></span> | Siokapesi Noema | Seattle | <a href="https://www.byui.edu/online" target="_blank">BYUI Online Learning</a></p>
+            |  <span id="currentDate"></span>
+                
+           </footer>
+       
+       
+        <script src="Js/myscript.js"></script>
+        
+    </Body>
 </html>
