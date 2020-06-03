c63bf1d72525624d4ce14b5767abad42c80fa385
NoemaSiokapesi18 <55466262+NoemaSiokapesi18@users.noreply.github.com>
2020-06-03 01:04:13 -0700

    commit

--- RazorPagesMovie/RazorPagesMovie/Pages/Movies/Index.cshtml.cs
+++ RazorPagesMovie/RazorPagesMovie/Pages/Movies/Index.cshtml.cs
@@ -27,7 +27,15 @@ namespace RazorPagesMovie.Pages.Movies
         public string MovieGenre { get; set; }
         public async Task OnGetAsync()
         {
-            Movie = await _context.Movie.ToListAsync();
+            {
+                var movies = from m in _context.Movie
+                             select m;
+                if (!string.IsNullOrEmpty(SearchString))
+                {
+                    movies = movies.Where(s => s.Title.Contains(SearchString));
+                }
+                Movie = await _context.Movie.ToListAsync();
+            }
         }
     }
 }
