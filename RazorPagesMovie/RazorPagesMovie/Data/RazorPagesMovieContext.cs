818dd7b286fe229a95dd3454cf8412ae9bcbd138
NoemaSiokapesi18 <55466262+NoemaSiokapesi18@users.noreply.github.com>
2020-06-03 23:00:25 -0700

    commit

--- RazorPagesMovie/RazorPagesMovie/Data/RazorPagesMovieContext.cs
+++ RazorPagesMovie/RazorPagesMovie/Data/RazorPagesMovieContext.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace RazorPagesMovie.Models
 {
     public class RazorPagesMovieContext : DbContext
     {
-        public RazorPagesMovieContext (DbContextOptions<RazorPagesMovieContext> options)
+        public RazorPagesMovieContext(DbContextOptions<RazorPagesMovieContext> options)
             : base(options)
         {
         }
