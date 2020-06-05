50f961ff98b7ef9377a9290bea4439994f41eed8
NoemaSiokapesi18 <55466262+NoemaSiokapesi18@users.noreply.github.com>
2020-06-05 00:25:59 -0700

    commit

--- RazorPagesMovie/RazorPagesMovie/Startup.cs
+++ RazorPagesMovie/RazorPagesMovie/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
-using RazorPagesMovie.Data;
+using RazorPagesMovie.Models;
 using System.Globalization;
 using Microsoft.AspNetCore.Localization;
 
