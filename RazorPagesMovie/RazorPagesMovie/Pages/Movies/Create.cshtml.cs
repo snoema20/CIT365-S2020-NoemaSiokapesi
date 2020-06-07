2de68a6930bc4a725c9e7ddfbd1b82ba258d37c3
NoemaSiokapesi18 <55466262+NoemaSiokapesi18@users.noreply.github.com>
2020-06-07 16:01:17 -0700

    commit

--- RazorPagesMovie/RazorPagesMovie/Pages/Movies/Create.cshtml.cs
+++ RazorPagesMovie/RazorPagesMovie/Pages/Movies/Create.cshtml.cs
@@ -1,9 +1,11 @@
-﻿// Unused usings removed.
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using RazorPagesMovie.Models;
-using System;
-using System.Threading.Tasks;
 
 namespace RazorPagesMovie.Pages.Movies
 {
