6e375097e92e148c045b410f105337f739bcdcfb
NoemaSiokapesi18 <55466262+NoemaSiokapesi18@users.noreply.github.com>
2020-06-07 22:07:38 -0700

    commit

--- MyScriptureJournal/RazorPagesMovie/Migrations/20200604054723_Initial.Designer.cs
+++ MyScriptureJournal/RazorPagesMovie/Migrations/20200604054723_Initial.Designer.cs
@@ -5,44 +5,53 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using RazorPagesMovie.Models;
+using My_Scripture_Journal.Models;
 
-namespace RazorPagesMovie.Migrations
+namespace My_Scripture_Journal.Migrations
 {
-    [DbContext(typeof(RazorPagesMovieContext))]
-    [Migration("20200604054723_Initial")]
+    [DbContext(typeof(My_Scripture_JournalContext))]
+    [Migration("20191102181812_Initial")]
     partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "3.1.4")
+                .HasAnnotation("ProductVersion", "3.0.0")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
-            modelBuilder.Entity("RazorPagesMovie.Models.Movie", b =>
+            modelBuilder.Entity("My_Scripture_Journal.Models.Scripture", b =>
                 {
                     b.Property<int>("ID")
                         .ValueGeneratedOnAdd()
                         .HasColumnType("int")
                         .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
-                    b.Property<string>("Genre")
+                    b.Property<string>("Book")
                         .HasColumnType("nvarchar(max)");
 
-                    b.Property<decimal>("Price")
+                    b.Property<decimal>("Chapter")
                         .HasColumnType("decimal(18,2)");
 
-                    b.Property<DateTime>("ReleaseDate")
+                    b.Property<DateTime>("DateAdded")
                         .HasColumnType("datetime2");
 
-                    b.Property<string>("Title")
+                    b.Property<string>("Note")
                         .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("NoteTitle")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Passage")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<decimal>("Verse")
+                        .HasColumnType("decimal(18,2)");
 
                     b.HasKey("ID");
 
-                    b.ToTable("Movie");
+                    b.ToTable("Scripture");
                 });
 #pragma warning restore 612, 618
         }
