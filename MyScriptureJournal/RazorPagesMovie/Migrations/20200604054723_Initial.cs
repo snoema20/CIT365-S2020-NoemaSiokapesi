d0e43a9968138f9abf8b2541594ab5234fb13b80
NoemaSiokapesi18 <55466262+NoemaSiokapesi18@users.noreply.github.com>
2020-06-07 22:05:05 -0700

    commit

---
+++ MyScriptureJournal/RazorPagesMovie/Migrations/20200604054723_Initial.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace My_Scripture_Journal.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Scripture",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NoteTitle = table.Column<string>(nullable: true),
+                    Book = table.Column<string>(nullable: true),
+                    Chapter = table.Column<decimal>(nullable: false),
+                    Verse = table.Column<decimal>(nullable: false),
+                    Note = table.Column<string>(nullable: true),
+                    Passage = table.Column<string>(nullable: true),
+                    DateAdded = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Scripture", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Scripture");
+        }
+    }
+}
