2146ac89fc71aad6dfaf8a95c0125020db3ee781
NoemaSiokapesi18 <55466262+NoemaSiokapesi18@users.noreply.github.com>
2020-06-07 23:54:58 -0700

    commit

---
+++ MyScriptureJournal/MyScriptureJournal/Data/MyScriptureJournalContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using My_Scripture_Journal.Models;
+
+namespace MyScriptureJournal.Models
+{
+    public class MyScriptureJournalContext : DbContext
+    {
+        public MyScriptureJournalContext (DbContextOptions<MyScriptureJournalContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<My_Scripture_Journal.Models.Scripture> Scripture { get; set; }
+    }
+}
---
+++ MyScriptureJournal/MyScriptureJournal/Pages/Scriptures/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MyScriptureJournal.Models;
+using My_Scripture_Journal.Models;
+
+namespace MyScriptureJournal.Pages.Scriptures
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MyScriptureJournal.Models.MyScriptureJournalContext _context;
+
+        public IndexModel(MyScriptureJournal.Models.MyScriptureJournalContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Scripture> Scripture { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Scripture = await _context.Scripture.ToListAsync();
+        }
+    }
+}
