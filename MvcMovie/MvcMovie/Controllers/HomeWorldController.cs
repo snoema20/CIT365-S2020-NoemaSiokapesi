b0196895be71c85e9430e06ab760c98c5112d0b4
NoemaSiokapesi18 <55466262+NoemaSiokapesi18@users.noreply.github.com>
2020-06-21 14:28:51 -0700

    commit

--- MvcMovie/MvcMovie/Controllers/HomeWorldController.cs
+++ MvcMovie/MvcMovie/Controllers/HomeWorldController.cs
@@ -15,10 +15,10 @@ namespace MvcMovie.Controllers
 
         // 
         // GET: /HelloWorld/Welcome/ 
-
-        public string Welcome()
+        // Requires using System.Text.Encodings.Web;
+        public string Welcome(string name, int ID = 1)
         {
-            return "This is the Welcome action method...";
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
         }
     }
 }
