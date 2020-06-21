781d1aa3d0a9ce9a9b25beae10a95aadf7040775
NoemaSiokapesi18 <55466262+NoemaSiokapesi18@users.noreply.github.com>
2020-06-21 09:49:58 -0700

    commit

--- MvcMovie/MvcMovie/Controllers/MoviesController.cs
+++ MvcMovie/MvcMovie/Controllers/MoviesController.cs
@@ -12,6 +12,9 @@ namespace MvcMovie.Controllers
     public class MoviesController : Controller
     {
         private readonly MvcMovieContext _context;
+        private object sTitle;
+
+        public object STitle { get => sTitle; set => sTitle = value; }
 
         public MoviesController(MvcMovieContext context)
         {
@@ -21,17 +24,29 @@ namespace MvcMovie.Controllers
         // GET: Movies
         // GET: Movies
         // GET: Movies
-        public async Task<IActionResult> Index(string id)
+        // GET: Movies
+        public async Task<IActionResult> Index(string movieGenre, string searchString)
         {
+            // Use LINQ to get list of genres.
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+
             var movies = from m in _context.Movie
                          select m;
-            if (!String.IsNullOrEmpty(id))
+
+            if (!string.IsNullOrEmpty(movieGenre))
             {
-                movies = movies.Where(s => s.Title.Contains(id));
+                movies = movies.Where(x => x.Genre == movieGenre);
             }
 
+            var movieGenreVM = new MovieGenreViewModel
+            {
+                Genres = new SelectList(await genreQuery.Distinct().ToListAsync()),
+                Movies = await movies.ToListAsync()
+            };
 
-            return View(await movies.ToListAsync());
+            return View(movieGenreVM);
         }
         // GET: Movies/Details/5
         public async Task<IActionResult> Details(int? id)
