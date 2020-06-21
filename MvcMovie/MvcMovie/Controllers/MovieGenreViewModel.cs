cb82289ba86ba159973131d837851448c1acfa77
NoemaSiokapesi18 <55466262+NoemaSiokapesi18@users.noreply.github.com>
2020-06-21 12:02:20 -0700

    commit

--- MvcMovie/MvcMovie/Controllers/MovieGenreViewModel.cs
+++ MvcMovie/MvcMovie/Controllers/MovieGenreViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 
 namespace MvcMovie.Controllers
 {
@@ -7,5 +8,22 @@ namespace MvcMovie.Controllers
         public SelectList Genres { get; set; }
         public System.Collections.Generic.List<Models.Movie> Movies { get; set; }
         public string SearchString { get; internal set; }
+    }
+}
+
+namespace MvcMovie.Models
+{
+    internal class Movie
+    {
+        public Movie()
+        {
+        }
+
+        public string Title { get; internal set; }
+        public DateTime ReleaseDate { get; internal set; }
+        public string Genre { get; internal set; }
+        public string Rating { get; internal set; }
+        public decimal Price { get; internal set; }
+        public int? Id { get; internal set; }
     }
 }
