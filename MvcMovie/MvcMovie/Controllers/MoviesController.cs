0f24ae9fea655fc3a40ce37f88774645ae644bdd
NoemaSiokapesi18 <55466262+NoemaSiokapesi18@users.noreply.github.com>
2020-06-20 22:55:02 -0700

    commit

--- MvcMovie/MvcMovie/Controllers/MoviesController.cs
+++ MvcMovie/MvcMovie/Controllers/MoviesController.cs
@@ -138,10 +138,10 @@ namespace MvcMovie.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var movie = await _context.Movie.FindAsync(id);
-            _context.Movie.Remove(movie);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            var movie = await _context.Movie
+    .FirstOrDefaultAsync(m => m.Id == id);
+            return View(movie);
+
         }
 
         private bool MovieExists(int id)
