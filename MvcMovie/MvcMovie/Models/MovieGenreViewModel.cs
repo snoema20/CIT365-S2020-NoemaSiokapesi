f2fb351db6ba1c59d0b4e7b1032a6ef18bb8eadc
NoemaSiokapesi18 <55466262+NoemaSiokapesi18@users.noreply.github.com>
2020-06-21 12:17:19 -0700

    commit

--- MvcMovie/MvcMovie/Models/MovieGenreViewModel.cs
+++ MvcMovie/MvcMovie/Models/MovieGenreViewModel.cs
@@ -5,7 +5,6 @@ namespace MvcMovie.Models
 {
     public class MovieGenreViewModel
     {
-        public List<Movie> Movies { get; set; }
         public SelectList Genres { get; set; }
         public string MovieGenre { get; set; }
         public string SearchString { get; set; }
