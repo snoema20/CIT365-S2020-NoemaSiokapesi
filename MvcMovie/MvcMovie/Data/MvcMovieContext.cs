ce4ef226ac3ef821e9fc498911e989afce63f10e
NoemaSiokapesi18 <55466262+NoemaSiokapesi18@users.noreply.github.com>
2020-06-21 19:22:28 -0700

    commit

--- MvcMovie/MvcMovie/Data/MvcMovieContext.cs
+++ MvcMovie/MvcMovie/Data/MvcMovieContext.cs
@@ -1,18 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿// Unused usings removed.
 using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;  // Enables public DbSet<Movie> Movie
 
-namespace MvcMovie.Models
+namespace MvcMovie.Data
 {
     public class MvcMovieContext : DbContext
     {
-        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
             : base(options)
         {
         }
 
-        public DbSet<MvcMovie.Models.Movie> Movie { get; set; }
+        public DbSet<Movie> Movie { get; set; }
     }
 }
