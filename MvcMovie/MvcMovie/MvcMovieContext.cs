5364c9a636372480a7dd1ca98d9fcc68e0605585
NoemaSiokapesi18 <55466262+NoemaSiokapesi18@users.noreply.github.com>
2020-06-21 19:34:33 -0700

    commit

---
+++ MvcMovie/MvcMovie/MvcMovieContext.cs
@@ -0,0 +1,6 @@
+﻿namespace MvcMovie
+{
+    internal class MvcMovieContext
+    {
+    }
+}
--- MvcMovie/MvcMovie/Startup.cs
+++ MvcMovie/MvcMovie/Startup.cs
@@ -8,28 +8,24 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MvcMovie.Data;
 using Microsoft.EntityFrameworkCore;
-using MvcMovie.Models;
 
 namespace MvcMovie
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
+            Environment = env;
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllersWithViews();
-
-            services.AddDbContext<MvcMovieContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("MvcMovieContext")));
-        }
+        
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
