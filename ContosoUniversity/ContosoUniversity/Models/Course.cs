c00799f2730054e06ce62cacede385942946d485
NoemaSiokapesi18 <55466262+NoemaSiokapesi18@users.noreply.github.com>
2020-07-07 00:19:03 -0700

    commit

--- ContosoUniversity/ContosoUniversity/Models/Course.cs
+++ ContosoUniversity/ContosoUniversity/Models/Course.cs
@@ -9,14 +9,16 @@ namespace ContosoUniversity.Models
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         [Display(Name = "Number")]
         public int CourseID { get; set; }
+
         [StringLength(50, MinimumLength = 3)]
         public string Title { get; set; }
+
         [Range(0, 5)]
         public int Credits { get; set; }
+
         public int DepartmentID { get; set; }
 
         public Department Department { get; set; }
-
         public ICollection<Enrollment> Enrollments { get; set; }
         public ICollection<CourseAssignment> CourseAssignments { get; set; }
     }
