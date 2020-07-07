f8ac78574eda2e86539e5725e013ee40c5595535
NoemaSiokapesi18 <55466262+NoemaSiokapesi18@users.noreply.github.com>
2020-07-06 23:05:45 -0700

    commit

--- ContosoUniversity/ContosoUniversity/Data/SchoolContext.cs
+++ ContosoUniversity/ContosoUniversity/Data/SchoolContext.cs
@@ -12,5 +12,24 @@ namespace ContosoUniversity.Models
         public DbSet<Student> Student { get; set; }
         public DbSet<Enrollment> Enrollment { get; set; }
         public DbSet<Course> Course { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
+        public DbSet<CourseAssignment> CourseAssignments { get; set; }
+        public object Courses { get; internal set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+            modelBuilder.Entity<Department>().ToTable("Department");
+            modelBuilder.Entity<Instructor>().ToTable("Instructor");
+            modelBuilder.Entity<OfficeAssignment>().ToTable("OfficeAssignment");
+            modelBuilder.Entity<CourseAssignment>().ToTable("CourseAssignment");
+
+            modelBuilder.Entity<CourseAssignment>()
+                .HasKey(c => new { c.CourseID, c.InstructorID });
+        }
     }
 }
