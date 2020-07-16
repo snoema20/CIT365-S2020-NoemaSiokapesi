68d292485eac78d20d186748f4954e6d360f995c
NoemaSiokapesi18 <55466262+NoemaSiokapesi18@users.noreply.github.com>
2020-07-15 23:23:30 -0700

    commit

---
+++ Sacrament Meeting Planner/Sacrament Meeting Planner/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sacrament_Meeting_Planner.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
