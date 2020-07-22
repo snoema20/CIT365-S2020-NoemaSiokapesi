1841b401fac61be7198d5a41fb08f28a7495b907
NoemaSiokapesi18 <55466262+NoemaSiokapesi18@users.noreply.github.com>
2020-07-21 22:08:34 -0700

    commit

---
+++ Question/src/DefaultPackage/App.java
@@ -0,0 +1,36 @@
+package DefaultPackage;
+
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String [] args) {
+		String q1 = "What color are apples?\n"
+				+ "(a)red/green\n{b)Oranges\n(C)Magenta\n";
+		String q2 = "What color are bananas?\n"
+				+ "(a)red/green\n(b)Yellow\n(c)Blue\n";
+		
+	Question [] questions = {
+			new Question(q1, "a"),
+			new Question(q2, "b")
+	};
+	takeTest(questions);
+	
+	}
+public static void takeTest(Question [] questions) {
+	int score = 0;
+	Scanner keyboardInput = new Scanner(System.in);
+	
+	for(int i = 0; i < questions.length; i++) {
+		System.out.println(questions[i].prompt);
+		String answer = keyboardInput.nextLine();
+		if(answer.equals(questions[i].answer)) {
+			score++;
+		}
+		}
+		System.out.println("You got " + score + "/" + questions.length);
+	}
+	}
+
+
+
